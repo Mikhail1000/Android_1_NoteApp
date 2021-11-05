a12931ef310ae6f7a2569cb34e72b30e7de64168
MT1000 <MT.1000@yandex.ru>
2021-11-05 23:38:19 +0500

    Добавлен второй фрагмент, отображается заметка, реализован переход с первого фрагмента

--- app/src/main/java/com/example/android1noteapp/Notes.java
+++ app/src/main/java/com/example/android1noteapp/Notes.java
@@ -1,9 +1,12 @@
 package com.example.android1noteapp;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class Notes {
+public class Notes implements Parcelable {
     private String nameNote;
     private String descriptionNote;
     Calendar dateCreate;
@@ -19,6 +22,23 @@ public class Notes {
         this.descriptionNote = descriptionNote;
         this.dateCreate = new GregorianCalendar();
     }
+
+    protected Notes(Parcel in) {
+        nameNote = in.readString();
+        descriptionNote = in.readString();
+    }
+
+    public static final Creator<Notes> CREATOR = new Creator<Notes>() {
+        @Override
+        public Notes createFromParcel(Parcel in) {
+            return new Notes(in);
+        }
+
+        @Override
+        public Notes[] newArray(int size) {
+            return new Notes[size];
+        }
+    };
 
     public String getNameNote() {
         return nameNote;
@@ -42,5 +62,16 @@ public class Notes {
 
     public void setDateCreate(Calendar dateCreate) {
         this.dateCreate = dateCreate;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(nameNote);
+        dest.writeString(descriptionNote);
     }
 }
--- app/src/main/java/com/example/android1noteapp/listNoteFragment.java
+++ app/src/main/java/com/example/android1noteapp/listNoteFragment.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,21 +16,15 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link listNoteFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class listNoteFragment extends Fragment {
 
     ArrayList<Notes> notes = new ArrayList<>();
 
-
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_INDEX = "index";
+    private static final String ARG_NOTES = "notes";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -39,32 +34,23 @@ public class listNoteFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment listNoteFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static listNoteFragment newInstance(String param1, String param2) {
+    /*public static listNoteFragment newInstance(int index, ArrayList<Notes> notes) {
         listNoteFragment fragment = new listNoteFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_INDEX, index);
+        args.putParcelableArrayList(ARG_NOTES, notes);
         fragment.setArguments(args);
         return fragment;
-    }
+    }*/
 
-    @Override
+    /*@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam1 = getArguments().getString(ARG_INDEX);
+            mParam2 = getArguments().getString(ARG_NOTES);
         }
-    }
+    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -85,12 +71,23 @@ public class listNoteFragment extends Fragment {
         notes.add(new Notes("Заголовок 3", "Описание заметки 3", new GregorianCalendar()));
 
         LinearLayout layoutView = (LinearLayout) view;
-        for (Notes note : notes) {
+        for (int i = 0; i < notes.size(); i++) {
             TextView tv = new TextView(getContext());
-            tv.setText(note.getNameNote());
+            tv.setText(notes. get(i).getNameNote());
             tv.setTextSize(30);
             layoutView.addView(tv);
+            final int position = i;
+            tv.setOnClickListener(v -> {
+                showNote(position);
+            });
         }
+    }
 
+    private void showNote(int index) {
+        NoteFragment noteFragment = NoteFragment.newInstance(index, notes);
+        requireActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, noteFragment)
+                .commit();
     }
 }
